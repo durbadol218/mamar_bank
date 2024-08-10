db2bcbf3dfa93eec5dd138658f5b63b65a100390
Durbadol Goswami <goswamidurbadol@gmail.com>
2024-08-10 09:07:30 +0600

    User profile update feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
+from .views import UserRegistrationView, UserLoginView, UserLogoutView, UserUpdateView
 # from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('register/',UserRegistrationView.as_view(), name='register'),
     path('login/',UserLoginView.as_view(), name='user_login'),
     path('logout/',UserLogoutView.as_view(), name='user_logout'),
-    
+    path('profile/',UserUpdateView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import FormView
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
@@ -37,3 +37,18 @@ class UserLogoutView(LogoutView):
     
     def get_success_url(self):
         return reverse_lazy('homepage')
+    
+
+class UserUpdateView(View):
+    template_name = 'profile.html'
+    
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request,self.template_name,{'form':form})
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the user's profile page
+        return render(request, self.template_name, {'form': form}) #automatically call korar jonno ai line lekhlam...jodi sobthik thaake tobe automatically kaaj korbe!
+    
