266853dd84d055592cf439d9c0f08a82e80f2113
Durbadol Goswami <99266434+durbadol218@users.noreply.github.com>
2024-08-13 10:56:38 +0600

    Update settings.py

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-=#=hvghi5lh+2!zk%t49nxp@3-$fbklv4^j7$_y1mh*^79nr-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
